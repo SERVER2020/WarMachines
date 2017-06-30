ef95afba2a62231b514f27a9f61f06f4abd2e4c2
Eric Siqueiros <rcsiqueiros1@gmail.com>
2017-06-30 02:10:27 -0700

    Added Player Movement

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public float playerHealth = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,17 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[System.Serializable]
+public class Boundry
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
 public class PlayerMovement : MonoBehaviour
 {
+    public float playerMovementSpeed = 0.0f;
+
+    private GameObject player = null;
+    private Rigidbody2D playerRigidBody;
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerRigidBody = player.GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    private void FixedUpdate()
+    {
+        Movement();
+        ProjectileFire();
+    }
+
+    private void Movement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        playerRigidBody.velocity = new Vector2(moveHorizontal, moveVertical) * playerMovementSpeed;
+        playerRigidBody.velocity.Normalize();
+
+    }
+
+    private void ProjectileFire()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
 		
 	}
